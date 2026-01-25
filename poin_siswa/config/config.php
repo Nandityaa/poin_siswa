0a686b527780288aa75222c6c68c5f00a01be36d
Nafa Nanditya <nafananditya2008@gmail.com>
2026-01-25 10:06:56 +0800

    yes

--- poin_siswa/config/config.php
+++ poin_siswa/config/config.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Koneksi DB gagal: " . $conn->connect_error);
 }
 
-$base_url = "/poin_siswaku";
+$base_url = "/poin_siswa";
 
 // timezone
 date_default_timezone_set("Asia/Makassar");
